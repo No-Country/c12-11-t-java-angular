1c419a63007dc83be99f401b820825b7c8f9d0f4
danielAlderete93 <daniel.a.alderete93@gmail.com>
2023-07-06 13:49:24 -0300

    delete: componente estrella

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,19 +8,26 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {NotFoundComponent} from './shared/pages/not-found/not-found.component';
 import {CardMenuMenuComponent} from '@modules/container/components/card-menu/card-menu.menu.component';
-import {StarsMenuComponent} from '@modules/container/components/star-menu/stars-menu.component';
 import {DeckMenuComponent} from '@modules/container/components/deck-menu/deck-menu.component';
 import {MenuComponent} from './modules/container/pages/menu/menu.component';
 import {
   ButtonFilterMenuComponent
 } from './modules/container/components/button-filter-menu/button-filter-menu.component';
-import { GroupButtonFilterMenuComponent } from './modules/container/components/group-button-filter-menu/group-button-filter-menu.component';
-import { InputSearchMenuComponent } from './modules/container/components/input-search-menu/input-search-menu.component';
-import { DescripcionProductoComponent } from './modules/container/pages/descripcion-producto/descripcion-producto.component';
-import { ButtonCounterComponent } from './modules/container/components/button-counter/button-counter.component';
-import { ButtonCounterProductOptionalComponent } from './modules/container/components/button-counter-product-optional/button-counter-product-optional.component';
-import { ProductOptionalComponent } from './modules/container/components/product-optional/product-optional.component';
-import { ProductDetailsModalComponent } from './modules/container/components/product-details-modal/product-details-modal.component';
+import {
+  GroupButtonFilterMenuComponent
+} from './modules/container/components/group-button-filter-menu/group-button-filter-menu.component';
+import {InputSearchMenuComponent} from './modules/container/components/input-search-menu/input-search-menu.component';
+import {
+  DescripcionProductoComponent
+} from './modules/container/pages/descripcion-producto/descripcion-producto.component';
+import {ButtonCounterComponent} from './modules/container/components/button-counter/button-counter.component';
+import {
+  ButtonCounterProductOptionalComponent
+} from './modules/container/components/button-counter-product-optional/button-counter-product-optional.component';
+import {ProductOptionalComponent} from './modules/container/components/product-optional/product-optional.component';
+import {
+  ProductDetailsModalComponent
+} from './modules/container/components/product-details-modal/product-details-modal.component';
 
 
 @NgModule({
@@ -28,7 +35,6 @@ import { ProductDetailsModalComponent } from './modules/container/components/pro
     AppComponent,
     NotFoundComponent,
     CardMenuMenuComponent,
-    StarsMenuComponent,
     DeckMenuComponent,
     MenuComponent,
     ButtonFilterMenuComponent,
