e7d734d8712a5895728641bb6e6c7ee1eb5cc8b0
danielAlderete93 <daniel.a.alderete93@gmail.com>
2023-07-06 13:29:14 -0300

    fix: card-menu (truncate)

--- frontend/src/app/modules/container/components/card-menu/card-menu.menu.component.ts
+++ frontend/src/app/modules/container/components/card-menu/card-menu.menu.component.ts
@@ -6,27 +6,25 @@ import {Component, Input} from '@angular/core';
   styleUrls: ['./card-menu.component.scss']
 })
 export class CardMenuMenuComponent {
-  /*
-  @Input() id!: number;
-  @Input() title!: string;
-  @Input() imageUrl!: string;
-  @Input() description!: string;
-  @Input() price!: number;
-*/
   @Input() product!: Product;
+  @Input() truncate: boolean = true;
 
   truncateTextMobile() {
-    if (this.product.description.length <= 60) {
+    if (this.product.description.length <= 60 || !this.truncate) {
       return this.product.description
     }
     return this.product.description.substring(0, 60) + '...'
   }
 
   truncateTextDektop() {
-    if (this.product.description.length <= 200) {
+    if (this.product.description.length <= 200 || !this.truncate) {
       return this.product.description
     }
     return this.product.description.substring(0, 200) + '...'
+  }
+
+  addCart() {
+    console.log("Se debe agregar al carrito")
   }
 }
 
