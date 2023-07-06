637e6a407d0d141d89308a8f5b47160cf50e2c42
JhordanVegaC <jhordan.vega.111@gmail.com>
2023-07-06 14:03:26 -0500

    fix: Arreglar app.module y container.module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,44 +7,11 @@ import {StoreModule} from '@ngrx/store';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {NotFoundComponent} from './shared/pages/not-found/not-found.component';
-import {CardMenuMenuComponent} from '@modules/container/components/card-menu/card-menu.menu.component';
-import {DeckMenuComponent} from '@modules/container/components/deck-menu/deck-menu.component';
-import {MenuComponent} from './modules/container/pages/menu/menu.component';
-import {
-  ButtonFilterMenuComponent
-} from './modules/container/components/button-filter-menu/button-filter-menu.component';
-import {
-  GroupButtonFilterMenuComponent
-} from './modules/container/components/group-button-filter-menu/group-button-filter-menu.component';
-import {InputSearchMenuComponent} from './modules/container/components/input-search-menu/input-search-menu.component';
-import {
-  DescripcionProductoComponent
-} from './modules/container/pages/descripcion-producto/descripcion-producto.component';
-import {ButtonCounterComponent} from './modules/container/components/button-counter/button-counter.component';
-import {
-  ButtonCounterProductOptionalComponent
-} from './modules/container/components/button-counter-product-optional/button-counter-product-optional.component';
-import {ProductOptionalComponent} from './modules/container/components/product-optional/product-optional.component';
-import {
-  ProductDetailsModalComponent
-} from './modules/container/components/product-details-modal/product-details-modal.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
-    CardMenuMenuComponent,
-    DeckMenuComponent,
-    MenuComponent,
-    ButtonFilterMenuComponent,
-    GroupButtonFilterMenuComponent,
-    InputSearchMenuComponent,
-    DescripcionProductoComponent,
-    ButtonCounterComponent,
-    ButtonCounterProductOptionalComponent,
-    ProductOptionalComponent,
-    ProductDetailsModalComponent
   ],
   imports: [
     BrowserModule,
