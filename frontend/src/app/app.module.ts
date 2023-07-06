15f206c3d1d79c7ec575a0a475ff4b82ee999207
danielAlderete93 <daniel.a.alderete93@gmail.com>
2023-07-06 13:45:16 -0300

    fix: modal producto + agregado a modulo

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,21 +6,29 @@ import {AppComponent} from './app.component';
 import {StoreModule} from '@ngrx/store';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {NotFoundComponent} from './shared/pages/not-found/not-found.component';
+import {NotFoundComponent} from '@shared/pages/not-found/not-found.component';
 import {CardMenuMenuComponent} from '@modules/container/components/card-menu/card-menu.menu.component';
 import {StarsMenuComponent} from '@modules/container/components/star-menu/stars-menu.component';
 import {DeckMenuComponent} from '@modules/container/components/deck-menu/deck-menu.component';
-import {MenuComponent} from './modules/container/pages/menu/menu.component';
+import {MenuComponent} from '@modules/container/pages/menu/menu.component';
 import {
   ButtonFilterMenuComponent
-} from './modules/container/components/button-filter-menu/button-filter-menu.component';
-import { GroupButtonFilterMenuComponent } from './modules/container/components/group-button-filter-menu/group-button-filter-menu.component';
-import { InputSearchMenuComponent } from './modules/container/components/input-search-menu/input-search-menu.component';
-import { DescripcionProductoComponent } from './modules/container/pages/descripcion-producto/descripcion-producto.component';
-import { ButtonCounterComponent } from './modules/container/components/button-counter/button-counter.component';
-import { ButtonCounterProductOptionalComponent } from './modules/container/components/button-counter-product-optional/button-counter-product-optional.component';
-import { ProductOptionalComponent } from './modules/container/components/product-optional/product-optional.component';
-import { ProductDetailsModalComponent } from './product-details-modal/product-details-modal.component';
+} from '@modules/container/components/button-filter-menu/button-filter-menu.component';
+import {
+  GroupButtonFilterMenuComponent
+} from '@modules/container/components/group-button-filter-menu/group-button-filter-menu.component';
+import {InputSearchMenuComponent} from '@modules/container/components/input-search-menu/input-search-menu.component';
+import {
+  DescripcionProductoComponent
+} from '@modules/container/pages/descripcion-producto/descripcion-producto.component';
+import {ButtonCounterComponent} from '@modules/container/components/button-counter/button-counter.component';
+import {
+  ButtonCounterProductOptionalComponent
+} from '@modules/container/components/button-counter-product-optional/button-counter-product-optional.component';
+import {ProductOptionalComponent} from '@modules/container/components/product-optional/product-optional.component';
+import {
+  ProductDetailsModalComponent
+} from '@modules/container/components/product-details-modal/product-details-modal.component';
 
 
 @NgModule({
