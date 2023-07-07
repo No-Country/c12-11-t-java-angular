e8329d3ee17338b06857b5778efcfc1e50fca90a
danielAlderete93 <daniel.a.alderete93@gmail.com>
2023-07-07 10:32:41 -0300

    refactor(web): Truncado de texto, funcion -> pipe

--- frontend/src/app/modules/container/components/card-menu/card-menu.menu.component.ts
+++ frontend/src/app/modules/container/components/card-menu/card-menu.menu.component.ts
@@ -9,20 +9,6 @@ export class CardMenuMenuComponent {
   @Input() product!: Product;
   @Input() truncate: boolean = true;
 
-  truncateTextMobile() {
-    if (this.product.description.length <= 60 || !this.truncate) {
-      return this.product.description
-    }
-    return this.product.description.substring(0, 60) + '...'
-  }
-
-  truncateTextDektop() {
-    if (this.product.description.length <= 200 || !this.truncate) {
-      return this.product.description
-    }
-    return this.product.description.substring(0, 200) + '...'
-  }
-
   addCart() {
     console.log("Se debe agregar al carrito")
   }
