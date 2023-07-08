cb3328732452d93834cb22acd5038638c8f1ca6f
Wilder <wilderruizrpg@gmail.com>
2023-07-07 23:28:10 -0500

    feat(controlador): se agregaron endpoints de la entidad Direccion
    Se implementaron los siguientes endpoints para realizar operaciones CRUD en la base de datos: GET /direcciones, POST /direcciones, DELETE /direcciones/{id}, PUT /direcciones/{id}

--- src/main/java/com/proyecto/controlador/Controlador.java
+++ src/main/java/com/proyecto/controlador/Controlador.java
@@ -1,6 +1,8 @@
 package com.proyecto.controlador;
 
+import com.proyecto.entidades.Direccion;
 import com.proyecto.entidades.Plato;
+import com.proyecto.servicio.direccionServIMPL.DSIMPL;
 import com.proyecto.servicio.platoServicioIMPL.PSIMPL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,6 +17,11 @@ public class Controlador {
 
     @Autowired
     private PSIMPL psimpl;
+    @Autowired
+    private DSIMPL dsimpl;
+
+
+    /* Entidad plato */
 
     @GetMapping
     @RequestMapping(value = "listaDePlatos", method = RequestMethod.GET)
@@ -62,6 +69,60 @@ public class Controlador {
         return ResponseEntity.ok().build();
 
     }
+
+    /*Entidad Direccion*/
+
+
+    @GetMapping
+    @RequestMapping(value = "listaDeDirecciones", method = RequestMethod.GET)
+    public ResponseEntity<?> listaDirecciones(){
+
+        List<Direccion> listaDirecciones = this.dsimpl.listaDeDirecciones();
+
+        return ResponseEntity.ok(listaDirecciones);
+
+    }
+
+    @PostMapping
+    @RequestMapping(value = "crearDireccion", method = RequestMethod.POST)
+    public ResponseEntity<?> crearDireccion(@RequestBody Direccion direccion){
+
+        Direccion direccionCreada = this.dsimpl.crearDireccion(direccion);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(direccionCreada);
+
+    }
+
+    @PutMapping
+    @RequestMapping(value = "modificarDireccion", method = RequestMethod.PUT)
+    public ResponseEntity<?> modificarDireccion(@RequestBody Direccion direccion){
+
+        Direccion direccionModificada = this.dsimpl.modificarDireccion(direccion);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(direccionModificada);
+
+    }
+
+    @GetMapping
+    @RequestMapping(value = "buscarDireccion/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> buscarDireccion(@PathVariable int id){
+
+        Direccion direccion = this.dsimpl.buscarDireccion(id);
+
+        return ResponseEntity.ok(direccion);
+
+    }
+
+    @DeleteMapping
+    @RequestMapping(value = "eliminarDireccion/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> eliminarDireccion(@PathVariable int id){
+
+        this.dsimpl.eliminarDireccion(id);
+
+        return ResponseEntity.ok().build();
+
+    }
+
 
 
 }
