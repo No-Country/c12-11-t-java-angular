370b6e53ae3e21790b85cc293352530823621c80
JhordanVegaC <jhordan.vega.111@gmail.com>
2023-07-13 21:51:57 -0500

    fix: truncar texto de descripción en los platos del home y agregar imágenes de platos

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const DOMAIN = 'localhost:8080';
 export const CONTEXT_PATH = 'VeggieDelivery';
-export const BASE_URL =  `https://${DOMAIN}/${CONTEXT_PATH}`
+export const BASE_URL =  `http://${DOMAIN}/${CONTEXT_PATH}`
