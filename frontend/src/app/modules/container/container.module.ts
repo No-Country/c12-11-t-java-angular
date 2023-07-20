ad76ea034905a1e4cab0b3c8e2997743999e7218
David Moreno <davidjmcxl@gmail.com>
2023-07-20 15:22:16 -0500

    feat:maquetado modal order confirmada

--- frontend/src/app/modules/container/container.module.ts
+++ frontend/src/app/modules/container/container.module.ts
@@ -57,6 +57,7 @@ import {
 import {NgxStripeModule} from 'ngx-stripe';
 
 
+
 @NgModule({
   declarations: [
     LayoutContainerComponent,
--- frontend/src/app/modules/container/pages/confirm-order/confirm-order.component.ts
+++ frontend/src/app/modules/container/pages/confirm-order/confirm-order.component.ts
@@ -1,100 +1,33 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {faAngleLeft, faArrowLeft, faPenToSquare} from '@fortawesome/free-solid-svg-icons';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {Store} from '@ngrx/store';
-import {PaymentService} from '../../../../shared/services/payment.service';
-import {PedidoRequest, PedidoService} from '@shared/services/pedido.service';
-
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { faAngleLeft, faArrowLeft, faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-confirm-order',
   templateUrl: './confirm-order.component.html',
   styleUrls: ['./confirm-order.component.scss']
 })
-export class ConfirmOrderComponent implements OnInit {
-
+export class ConfirmOrderComponent {
   faAngleLeft = faAngleLeft;
-  faArrowLeft = faArrowLeft;
-  faPenToSquare = faPenToSquare;
-  public stripeId: string = "";
-  public valueMethod: string = "0";
-  public textButtom: string = "";
-  public modal = inject(NgbModal);
-  public fecha: string = new Date().toLocaleString();
-  public fechaEstimada: string = "";
-  public pedido!: PedidoRequest;
-  public cardsRegister: any = {
-    name: "david",
-    brand: "visa"
-  };
+  faArrowLeft=faArrowLeft;
+  faPenToSquare=faPenToSquare;
 
-  private router = inject(Router);
-  private store = inject(Store);
-  private paymentService = inject(PaymentService);
-  private pedidoService = inject(PedidoService);
+  public valueMethod:string="0";
+  public textButtom:string="";
+  private router= inject( Router );
+  public modal= inject( NgbModal );
+  public cardsRegister:any[]=[{
+    numero:"ds442dff",
+    name:'Juan Perez',
+  },
+  {
+    numero:"123433",
+    name:'luis Perez',
+  }]
 
-  ngOnInit(): void {
-
-
-    this.getFechas();
-    this.getPedido();
-    console.log(this.fecha, this.fechaEstimada);
-    this.store.select("card").subscribe(resp => {
-
-      if (!resp) {
-        return
-      }
-      this.cardsRegister = resp.card;
-    })
-
-  }
-
-  getPedido() {
-    this.store.select("pedido").subscribe(resp => {
-      const {stripeId} = resp;
-      this.stripeId = stripeId;
-      const pedido: PedidoRequest = {
-        ...resp,
-        usuarioId: 1,
-        fechaPedido: this.fecha,
-        fechaEntrega: this.fechaEstimada,
-        fechaEntregaEstimada: this.fechaEstimada,
-        estadoPedidoId: 1
-
-      }
-
-
-      this.pedido = pedido;
-
-    })
-  }
-
-  confirm() {
-
-    this.pedidoService.crearPedido(this.pedido).subscribe(() => {
-
-      this.paymentService.confirmar(this.stripeId).subscribe(confirm => {
-        console.log(confirm);
-
-      })
-    })
-
-  }
-
-  goBack() {
+  goBack(){
     this.router.navigateByUrl('/container/pay');
-
-  }
-
-  getFechas() {
-
-    let fecha = new Date();
-    let fechaEstimada = new Date();
-    fechaEstimada.setHours(fecha.getHours() + 1);
-    this.fecha = fecha.toLocaleString();
-    this.fechaEstimada = fechaEstimada.toLocaleString();
-
 
   }
 }
