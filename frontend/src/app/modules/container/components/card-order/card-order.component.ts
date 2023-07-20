57e417e670d9b31bb8b39e8ce6aeb91983ff51b9
danielAlderete93 <daniel.a.alderete93@gmail.com>
2023-07-20 12:19:33 -0300

    feat(web): Agrego integracion de carrito con pedido detalle(api) y pedido(api: solo estado nuevo).

--- frontend/src/app/modules/container/components/card-order/card-order.component.ts
+++ frontend/src/app/modules/container/components/card-order/card-order.component.ts
@@ -6,6 +6,7 @@ import {Order} from "../../../../store/models/order.model";
 import {Plate} from "@shared/interfaces/plate.interface";
 import {CartFacade} from "@shared/services/facades/cart.facade";
 
+import {CartFacade} from "@shared/services/facades/cart.facade";
 
 @Component({
   selector: 'app-card-order',
@@ -42,10 +43,10 @@ export class CardOrderComponent implements OnInit {
       this.cartFacade.updateOrder(order)
     } else {
       this.cartFacade.removeOrderToCart(order)
-
     }
-  }
 
+
+  }
 
 }
 
--- frontend/src/app/store/reducers/app.reducer.ts
+++ frontend/src/app/store/reducers/app.reducer.ts
@@ -1,7 +1,19 @@
 import {createReducer} from '@ngrx/store';
+import {CartState} from "../models/cart-state.model";
 import {AppState} from "../models/app-state.model";
 
 export const initialState: AppState = {
+  cart: {
+    id: 0,
+    orders: [],
+    total: 0,
+    state: CartState.New
+  },
+  user: {
+    userId: -1,
+    username: '',
+    token: ''
+  },
   loading: false,
   error: null
 };
