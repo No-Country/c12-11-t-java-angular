a5e84fcc0b4e864874be789d27d8b1f73ff42d88
JhordanVegaC <jhordan.vega.111@gmail.com>
2023-07-25 21:11:30 -0500

    chore: configurar con backend desplegado

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,3 +1,5 @@
-export const DOMAIN = 'localhost:8080';
+//export const DOMAIN = 'localhost:8080';
+export const DOMAIN ='c12-11-t-java-angular-backend-production.up.railway.app'
 export const CONTEXT_PATH = 'VeggieDelivery';
-export const BASE_URL =  `http://${DOMAIN}/${CONTEXT_PATH}`
+//export const BASE_URL =  `http://${DOMAIN}/${CONTEXT_PATH}`
+export const BASE_URL =  `https://${DOMAIN}/${CONTEXT_PATH}`
